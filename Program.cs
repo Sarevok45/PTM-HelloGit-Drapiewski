77c3de8afd6abf7fccd6fa85214b7b523dcf9c90
Krzysztof <kfigiel.projekty@gmail.com>
2016-03-15 08:30:32 +0100

    Test

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM_HelloGit_Drapiewski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 43;
+            int y = 23;
+        }
+    }
+}
